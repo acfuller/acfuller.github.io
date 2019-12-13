d73d31277562b10a2ab8fa9d5c8ea2dfbae5d198
Adam <Adam@DESKTOP-D8VDAPD>
2019-12-12 20:17:24 -0600

    updated

---
+++ Bugz-B-Gon/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Bugz_B_Gon.MvcApplication" Language="C#" %>
---
+++ Bugz-B-Gon/Models/ProjectModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Bugz_B_Gon.Models
+{
+    public class ProjectModel
+    {
+        public int ID { get; set; }
+        public string ProjectName { get; set; }
+
+        public string Comments { get; set; }
+
+        public string Status { get; set; }
+
+        public string Date { get; set; }
+    }
+
+    public class ProjectDBContext : DbContext
+    {
+        public DbSet<ProjectModel> Projects { get; set; }
+    }
+}
